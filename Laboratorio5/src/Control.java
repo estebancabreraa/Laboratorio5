61a30e06aee965a6915f2e6444454527347639a5
David <David@LAPTOP-4MAV6DKP>
2017-11-03 09:44:17 -0600

    Ingrese metodos a clase control

--- Laboratorio5/src/Control.java
+++ Laboratorio5/src/Control.java
@@ -8,7 +8,66 @@
  *
  * @author David Soto y Esteban Cabrera
  */
+
 public class Control {
+    //Atributos
     private Tanque[] tanques = new Tanque[10];
-    //Hola //Hola//cx\z
+    //Constructor
+    public Control(){
+        
+    }
+    //Metodos
+    /**
+     * 
+     * @return 
+     */
+    public Tanque crearTanque(){
+       Tanque nuevo = new Tanque();
+       return nuevo;
+    }
+    /**
+     * 
+     */
+    public void ingresarTanque(){
+        
+    }
+    /**
+     * 
+     * @return 
+     */
+    public String Alerta(){
+        
+        String alerta = "ALERTA";
+        return alerta;
+    }
+    /**
+     * 
+     */
+    public void cerrarValvulas(){
+        
+    }
+    /**
+     * 
+     * @param posicion
+     * @return 
+     */
+    public Tanque buscarTanque(int posicion){
+        
+        Tanque busqueda = new Tanque();
+        return busqueda;
+    }
+    /**
+     * 
+     * @return 
+     */
+    public int calcularCantidadValvulasCilindricas(){
+        
+        int cantidad = 1;
+        return cantidad;
+    }
+    
+    public int calcularCantidadAguaTotal(){
+        int cantidadTotal = 0;
+        return cantidadTotal;
+    }
 }
